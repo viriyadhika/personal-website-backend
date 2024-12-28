4437b2075eb187e4b0eb06d092eb4a7aa4c2146c
Viriyadhika <viriya.dhika0@gmail.com>
2024-12-28 15:46:07 +0800

    docker

--- consumer.sh
+++ consumer.sh
@@ -1,6 +1,7 @@
 # Wait for kafka to fully boot
 sleep 15
 echo 'Wakeyy'
+alembic upgrade head
 python3 -u -m app setup
 # Unbuffered so that the print output is not saved to buffer but displayed straight away
 python3 -u -m app consumer
